bf1d790a1cfab4c47233a19000668a6c16bc9565
PRAJAPATI HARSH <harshprajapati8583@gmail.com>
2024-09-16 16:37:57 +0530

    production

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import LoginForm from "../Components/LoginForm";
+import LoginForm from "../Components/loginForm";
 import { useAuth } from "../context/contextapi";
 
 const LoginPage = () => {
